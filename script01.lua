6978acd8bf7acd9d3d2792f22c54275d4db90a34
4Lxxxx <75991811+4Lxxxx@users.noreply.github.com>
2020-12-17 12:48:50 -0300

    Create script01.lua

---
+++ script01.lua
@@ -0,0 +1,14 @@
+-- O comentario da Lua diferente de outras linguagens e feita usando 2 hífen, após isso pode ser feito o comentario.
+
+-- Criação de uma variavél
+texto = "Olá Mundo"
+-- Retorna o valor no console.
+print(texto)
+
+-- Alterando o valor de uma variavél pré-definida
+texto = "Esse texto foi alterado'
+print(texto)
+-- Concatenando variaveis
+questao = "Qual país você mora: "
+resposta = "Brasil"
+print(questao + resposta)
